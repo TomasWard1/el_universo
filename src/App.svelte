122e2c15af8cf720490c9f63c591b7226ee3829c
Tomas Ward <tomas@familiaward.com.ar>
2024-04-10 11:19:58 -0300

    comeinzo carga de datos

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,15 @@
 <!-- Script JS -->
 <script>
+    import * as d3 from "d3";
+    import { onMount } from "svelte";
+    
+    let alumnos = [];
+
+    onMount(() => {
+        d3.csv("./data/data.csv", d3.autoType).then((data) => {
+            console.log(data);
+        });
+    });
 </script>
 
 <!-- Estructura contenido HTML -->
